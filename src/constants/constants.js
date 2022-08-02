23140fd0cad35fd712c265cbfd5cc0d56f63509b
Daniellios <danielo49134@gmail.com>
2022-08-02 12:52:14 +0300

    Img Changes

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: "Task Manager",
     description: "Task manager to keep track of things you need to do",
-    image: "/images/taskmanager.png",
+    image: "/images/TaskManager.png",
     tags: [
       "JavaScript",
       "TypeScript",
