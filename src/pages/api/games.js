b56d8b89620e15536ecebf982e81ac98bfce1bb2
Daniellios <danielo49134@gmail.com>
2022-08-10 20:06:50 +0300

    path

--- src/pages/api/games.js
+++ src/pages/api/games.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import fs from "fs"
-const filename = "/htmls/hiddenObjGame.html"
+const filename = "/htmls/HiddenObjGame.html"
 export default async function api(req, res) {
   res.setHeader("Content-Type", "text/html; charset=utf-8")
 
